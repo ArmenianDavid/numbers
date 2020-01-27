31d07c9eff526768c11adb82c34c1817b6983259
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-27 22:30:34 +0400

    style : added bg and some styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends React.Component{
     return (
     <div className='body-inner'>
        <div className='body-container'>
-          Hello
+          <div className='header-container'>
+             <h1 className='header-title'>numbers</h1>
+             <h2 className='header-description'>What tales do your metrics tell?</h2>
+          </div>
        </div>
     </div>
     ); 
