b5d7decb006f58bb2d9dc5a05007c9e83ee42637
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-29 13:19:01 +0400

    style : added some styling

--- src/Components/Description/Part.js
+++ src/Components/Description/Part.js
@@ -20,10 +20,18 @@ class DatePart extends React.Component{
   render(){
     return(
       <div className='part'>
-         <h2 className='part-title'>{this.props.title}</h2>
+         <div className='part-title-container'>
+           <div>
+              <h2 className='part-title'>{this.props.title}</h2>
+           </div>
+         </div>
          <div className='part-box'>
-            <div className='box-link'>numbersapi.com/{this.props.api}</div>
-            <div className='box-text'>{this.state.data}</div>
+           <div className='box-link-container'>
+             <div className='box-link'>numbersapi.com/{this.props.api}</div>
+           </div> 
+           <div className='box-text-container'>
+             <div className='box-text'>{this.state.data}</div>
+           </div>
          </div>
       </div>
     )
