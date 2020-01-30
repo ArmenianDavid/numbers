245db1e60e2e5fc728f7b99481468df9407cb30b
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-30 20:52:48 +0400

    feat : added TryIt component

---
+++ src/Components/TryIt/TryIt.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TryIt = () =>{
+
+  return(
+    <div className='try-container'>
+      <div className='try'>
+        <div className='left-div'>
+
+        </div>
+        <div className='right-div'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TryIt
