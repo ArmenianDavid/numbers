1f0e65fa83f3b773904efbace33ab833bb89b22e
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-31 18:04:38 +0400

    feat : added onkeydoun and input part

--- src/Components/TryIt/TryIt.js
+++ src/Components/TryIt/TryIt.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react'
 
 const TryIt = () => {
   // const { value , setValue } = useState('/2/math')
-     const [ link , setLink ] = useState('');
+     const [ link , setLink ] = useState('6/math');
      const [ data , setData ] = useState('');
+     const [ input , setInput ] = useState('')
 
   const handleChangeInput = (e) =>{
-     setLink('numbersapi.com/'+e.target.value)
+     setInput(e.target.value)
+     console.log(input)
   }
 
   useEffect(() => {
-      fetch("http://numbersapi.com/6/math")
+      console.log(link)
+      fetch(`http://numbersapi.com/${link}`)
           .then(res => {
               console.log(res);
               return  res.text();
@@ -22,13 +25,28 @@ const TryIt = () => {
           });
   },[]);
 
+  const onEnter = (e) =>{
+     e.persist();
+     console.log(e)
+     if(e.key === 'Enter'){
+       console.log(input)
+       setLink(input)
+     }
+  }
+  console.log('data' , data )
+  console.log('input ', input )
+  console.log('link' , link);
   return(
     <div className='try-container'>
       <div className='try'>
         <div className='left-div'>
            <div className='top-part'>
              <div>
-               numbersapi.com/<input type='text' defaultValue='/2/math' onChange={handleChangeInput} />
+               numbersapi.com/<input 
+                                 type='text' 
+                                 defaultValue={link} 
+                                 onChange={handleChangeInput} 
+                                 onKeyDown={(e) => onEnter(e)} />
              </div>
            </div>
            <div className='bottom-part'>
